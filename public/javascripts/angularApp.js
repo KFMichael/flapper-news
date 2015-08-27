7395edefd6b470a3e9b43e42e0c34ff6c9491aad
KFMichael <kangahmichael@gmail.com>
2015-08-27 17:12:29 +0200

    angularJS + node avec serveur express

---
+++ public/javascripts/angularApp.js
@@ -0,0 +1,28 @@
+//on declare une nouvelle angularApp Angular nommée flapperNew 
+//on inclut le module ui.router comme dependance externe 
+var angularApp = angular.module('flapperNews', ['ui.router']);
+
+
+/*
+ici on configure l'application afin de bien la router :3
+ */
+angularApp.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider) {
+
+        $stateProvider
+            .state('home', { // le nom de l'état -->  HOME 
+                url: '/home', // il est appelé via cette url
+                templateUrl: '/home.html', //voila ce qu'on affiche quand on est dans cet état
+                controller: 'MainCtrl' // il es controlé par MainCtrl
+            })
+            .state('posts', { // le nom de l'état --> POSTS 
+                url: '/posts/{id}', // il es appeleé via cet url
+                templateUrl: '/posts.html', // voila ce qu'on affiche 
+                controller: 'PostsCtrl' // il es controlé par PostsCtrl
+            });
+
+        $urlRouterProvider.otherwise('home'); // si on ne sais pas ou on s'en va on reviens a home :p 
+    }
+]);
---
+++ public/javascripts/controllers/MainCtrl.js
@@ -0,0 +1,46 @@
+//le controlleur principale qui va se charger de la vue principale afin de gerer le two-way data biding 
+//
+// la variable scope sert de pont entre les controlleur angular et les templates angular : si tu veux 
+// que quelque ( function , variable )  chose soit accessible dans ta template 
+// attache le à scope :) 
+// 
+// on injecte ici le service 'posts' au niveau du controlleur afin d'acceder a ses données 
+// 
+angularApp.controller('MainCtrl', ['$scope', 'posts', function($scope, posts) {
+    $scope.test = 'Hello world!'; // pour affincher un message de test 
+
+    $scope.posts = posts.posts; // on lit la variable posts de notre vue au tableau qui contient les données à manipuler qui est dans le services posts.js 
+    // ainsi toutes les modifications effectuees dans la vue sera automatiquement appliquées aux services et seront donc vue pas tous les autre controlleur qui utilisent le services
+
+
+
+    // fonction qui permet d'ajouter un post en entrant son titre .  
+    $scope.addPost = function() {
+        if (!$scope.title || $scope.title === '') {
+            return;
+        } // afin de ne pas laisser des utilisateurs inconscients a entrer des choses vides 
+      
+        $scope.posts.push({
+            title: $scope.title,
+            link: $scope.link,
+            upvotes: 8,
+            comments: [{ // ajouter de faux commentaires , afin de voir la disposition 
+                author: 'Joe',
+                body: 'Cool post!',
+                upvotes: 7
+            }, {
+                author: 'Bob',
+                body: 'Great idea but everything is wrong!',
+                upvotes: 14
+            }]
+        });
+        $scope.title = '';
+        $scope.link = '';
+    };
+
+
+    $scope.incrementUpvotes = function(post) {
+        post.upvotes += 1;
+    };
+
+}]);
